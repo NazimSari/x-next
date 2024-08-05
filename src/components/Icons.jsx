4e69108a50e12a985698fb7949b70114433b0b79
Nazim <yasarnazimsari@gmail.com>
2024-08-05 22:03:27 +0300

    update deletePost function in icons component to handle post deletion

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -17,7 +17,7 @@ import {
 } from "firebase/firestore";
 import { app } from "@/firebase";
 import { useEffect, useState } from "react";
-export default function Icons({ id }) {
+export default function Icons({ id, uid }) {
   const { data: session } = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]);
@@ -48,6 +48,22 @@ export default function Icons({ id }) {
       likes.findIndex((like) => like.id === session?.user?.uid) !== -1
     );
   }, [likes]);
+  const deletePost = async () => {
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      if (session?.user?.uid === uid) {
+        deleteDoc(doc(db, "posts", id))
+          .then(() => {
+            console.log("Document successfully deleted!");
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.log("Error removing document: ", error);
+          });
+      } else {
+        alert("You are not authorized to delete this post");
+      }
+    }
+  };
   return (
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
@@ -69,8 +85,12 @@ export default function Icons({ id }) {
           </span>
         )}
       </div>
-
-      <HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
+      {session?.user?.uid === uid && (
+        <HiOutlineTrash
+          onClick={deletePost}
+          className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100"
+        />
+      )}
     </div>
   );
 }
